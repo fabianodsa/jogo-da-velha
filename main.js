612d8de89497bacea1bdaf88dce3d0c769c85fb4
fabianodsa <fabyanoduarte@hotmail.com>
2023-01-05 13:47:22 -0300

    Função que inicia o jogo, com o array vazio e reconhece os botões que foram clicados, criada

---
+++ main.js
@@ -0,0 +1,29 @@
+const currentPlayer = document.querySelector(".currentPlayer");
+
+let selected;
+let player = "X";
+
+// posições possíveis para um ganhador
+let positions = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+
+function init() {
+  selected = [];
+
+  currentPlayer.innerHTML = `JOGADOR DA VEZ: ${player}`;
+
+  document.querySelectorAll(".game button").forEach((item) => {
+    item.innerHTML = "";
+    item.addEventListener("click", newMove);
+
+    console.log(init);
+  });
+}
