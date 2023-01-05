a5d07fda9c5c80a0931910e73b0930a54290c85b
fabianodsa <fabyanoduarte@hotmail.com>
2023-01-05 14:34:18 -0300

    Projeto finalizado

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const currentPlayer = document.querySelector(".currentPlayer");
 let selected;
 let player = "X";
 
-// posições possíveis para um ganhador
+// posições possíveis para ter um ganhador
 let positions = [
   [1, 2, 3],
   [4, 5, 6],
@@ -23,7 +23,48 @@ function init() {
   document.querySelectorAll(".game button").forEach((item) => {
     item.innerHTML = "";
     item.addEventListener("click", newMove);
-
-    console.log(init);
   });
+}
+
+init();
+
+function newMove(e) {
+  const index = e.target.getAttribute("data-i");
+  e.target.innerHTML = player;
+  e.target.removeEventListener("click", newMove);
+  selected[index] = player;
+
+  setTimeout(() => {
+    check();
+  }, [100]);
+
+  player = player === "X" ? "O" : "X";
+  currentPlayer.innerHTML = `JOGADOR DA VEZ: ${player}`;
+}
+
+function check() {
+    let playerLastMove = player === "X" ? "O" : "X";
+    
+    const items = selected
+    .map((item, i) => [item, i])
+    .filter((item) => item[0] === playerLastMove)
+    .map((item) => item[1]);
+    
+    for (pos of positions) {
+        if (pos.every((item) => items.includes(item))) {
+          alert("O JOGADOR '" + playerLastMove + "' GANHOU!");
+          init();
+          return;
+        }
+      }
+    
+      if (selected.filter((item) => item).length === 9) {
+        alert("DEU EMPATE!");
+        init();
+        return;
+      }
+    }
+    
+function resetaOJogo (){
+    init();
 }
